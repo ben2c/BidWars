295c6367c6875093f7b65d25d5ee28199bacfa46
ben2c <bencheng200@gmail.com>
2021-04-12 17:58:20 -0500

    controller finished for user to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,28 @@
 class UsersController < ApplicationController
-    protect_from_forgery with: :exception
-    include ApplicationHelper
 
     def new 
+        @user = User.new
     end 
 
-    def create 
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            #log in user
+            session[:user_id] = @user.id
+            #redirect to the show page
+            redirect_to @user
+ 
+        else
+            render :new 
+        end
     end
 
     def show 
     end
 
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
